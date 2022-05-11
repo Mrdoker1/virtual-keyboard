264ebbc6ebcada6ae31863fd10c6d31e5867dcd1
viachaskul <fixrapdoK@gmail.com>
2022-05-11 14:03:30 +0300

    fix: console errors fixed

--- virtual-keyboard/script/input.js
+++ virtual-keyboard/script/input.js
@@ -9,7 +9,10 @@ export class Input {
         let input = document.createElement('div');
         input.classList.add('input');
         input.innerHTML = `
-        <p>Начните печатать или выберите область ввода</p>
+        <div class="description">
+           <p>Начните печатать или выберите область ввода</p>
+           <a href="https://github.com/Mrdoker1/virtual-keyboard/pull/1">Pull Request Link</a>
+        </div>
         <textarea id="input" rows="4" cols="50"></textarea>
             <div class="info">
                 <p>Клавиатура создана в операционной системе: ${this.operationSystem}</p>
